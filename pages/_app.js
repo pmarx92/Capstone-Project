4b36f05566514d903638f09cec268a2cbe341d5f
Patrick <pamarx92@gmail.com>
2022-10-25 11:01:48 +0200

    imported the layout file

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import GlobalStyle from "../components/GlobalStyle";
+import Layout from "../pages/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
