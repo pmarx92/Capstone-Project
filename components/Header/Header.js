c9c299478a0d7b23ccf69f35bb04b6acb22c8558
Patrick <pamarx92@gmail.com>
2022-10-25 10:41:34 +0200

    changed the Header Headline

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,14 +3,14 @@ import styled from "styled-components";
 function Header() {
   return (
     <Background>
-      <StyledHeadline>Capstone prep</StyledHeadline>
+      <StyledHeadline>Capstone Project</StyledHeadline>
     </Background>
   );
 }
 
 const Background = styled.div`
   background-color: lightgreen;
-  padding: 10px;
+  padding: 20px;
   box-shadow: 0 0 10px grey;
 `;
 const StyledHeadline = styled.h1`
