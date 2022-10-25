ef8e9cb3a7c27f669248e004e8bdb9df4989d101
Patrick <pamarx92@gmail.com>
2022-10-25 10:38:19 +0200

    added default padding and margin of 0

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -8,11 +8,13 @@ const GlobalStyle = createGlobalStyle`
   
       * {
           box-sizing: border-box;
+          padding: 0;
+          margin: 0;
       }
   
       body {
           font-family: 'Noto Sans', sans-serif;
-          background-color: var(--background-primary)
+          background-color: var(--background-primary);
           color: var(--text-primary);
       }
   `;
