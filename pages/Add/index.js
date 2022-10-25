49a0434be4fe25bdd16228f5d8d4fdb3c8df12e4
Patrick <pamarx92@gmail.com>
2022-10-25 10:52:37 +0200

    added all 3 pages

---
+++ pages/Add/index.js
@@ -0,0 +1,7 @@
+export default function Add() {
+  return (
+    <div>
+      <h1>This is the Add Page - Under Construction</h1>;
+    </div>
+  );
+}
---
+++ pages/List/index.js
@@ -0,0 +1,7 @@
+export default function List() {
+  return (
+    <div>
+      <h1>This is the List Page - Under Construction</h1>;
+    </div>
+  );
+}
