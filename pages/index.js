102ad50b8853496a2e2099fc598d5f3f30dbd85f
Patrick <pamarx92@gmail.com>
2022-10-25 10:43:17 +0200

    added the navigation to the main page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styled from "styled-components";
 import Header from "../components/Header/Header";
+import Navigation from "../components/Navigation/Navigation";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
       <Main></Main>
+      <Navigation />
     </div>
   );
 }
