f68f317a7ca938c80a26fb50adec84c8f7506c09
Patrick <pamarx92@gmail.com>
2022-10-28 12:14:13 +0200

    added leaflet inside the form page

---
+++ components/map/Map.js
@@ -0,0 +1,35 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+import styled from "styled-components";
+
+export default function Map() {
+  return (
+    <>
+      <Headline>Leaflet Map</Headline>
+      <MapContainer
+        center={[58.034125450605316, 7.454502477686363]}
+        zoom={11}
+        scrollWheelZoom={false}
+        onClick={(event) => getCoord(event.latlng)}
+        style={{
+          width: "70%",
+          height: "37rem",
+          borderRadius: "40px",
+          boxShadow: "0 0 10px black",
+          margin: "3rem auto",
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        ></TileLayer>
+      </MapContainer>
+    </>
+  );
+}
+
+const Headline = styled.h1`
+  margin-top: 3rem;
+  text-align: center;
+`;
