eda90033aae3b1b89c7bf7f78281bb8f5bed12d3
Patrick <pamarx92@gmail.com>
2022-11-03 11:22:01 +0100

    added Schema for mongoose

---
+++ models/Cards.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const CardsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Add a Name"],
+    unique: true,
+    trim: true,
+    maxlength: [15, "The name cannot be more than 15 characters!"],
+  },
+  weight: {
+    type: String,
+    required: [true, "Please add the Weight."],
+    maxlength: [2, "Do not go above 2!"],
+  },
+  length: {
+    type: String,
+    required: [true, "Please add the Length."],
+    maxlength: [2, "Do not go above 2!"],
+  },
+  location: {
+    type: String,
+    required: [true, "Please add the Location."],
+    maxlength: [15, "The name cannot be more than 15 characters!"],
+  },
+  date: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.models.Cards || mongoose.model("Cards", CardsSchema);
