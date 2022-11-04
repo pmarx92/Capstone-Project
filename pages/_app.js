79c1a4a02aa598991d65a855d931a1b55dac8f46
Patrick <pamarx92@gmail.com>
2022-11-04 07:26:12 +0100

    added data fetching

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,49 @@ import Layout from "../pages/Layout";
 import "react-datepicker/dist/react-datepicker.css";
 import useLocalStorage from "../components/hooks/LocalStorage";
 import { useState } from "react";
+import { useEffect } from "react";
+/* import data from "../components/DataFetching"; */
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, data }) {
   const [startDate, setStartDate] = useState(new Date());
   const [fishList, setFishList] = useLocalStorage("fishList", []);
   const [fishName, setFishName] = useState("");
   const [fishWeight, setFishWeight] = useState();
   const [fishLength, setFishLength] = useState();
   const [fishLocation, setFishLocation] = useState("");
+
+  const [fetchedData, setFetchedData] = useState([]);
+
+  async function fetchAPI() {
+    const request = await fetch("http://localhost:3000/api/formdata");
+    const data = await request.json();
+    console.log(data.data);
+    setFetchedData(data.data);
+    const array = [
+      {
+        name: "Patrick",
+        alter: 30,
+      },
+      {
+        name: "Celine",
+        alter: 24,
+      },
+    ];
+    console.log(
+      array[0].name +
+        " ist " +
+        array[0].alter +
+        " Jahre alt und " +
+        array[1].name +
+        " ist " +
+        array[1].alter +
+        " Jahre alt"
+    );
+  }
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
   return (
     <>
@@ -30,6 +65,7 @@ function MyApp({ Component, pageProps }) {
           setFishLength={setFishLength}
           fishLocation={fishLocation}
           setFishLocation={setFishLocation}
+          fetchedData={fetchedData}
         />
       </Layout>
     </>
