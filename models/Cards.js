3c99621898204c61821320626dc9733a4570ef88
Patrick <pamarx92@gmail.com>
2022-11-05 16:45:12 +0100

    changed strings to numbers

--- models/Cards.js
+++ models/Cards.js
@@ -9,12 +9,12 @@ const CardsSchema = new mongoose.Schema({
     maxlength: [15, "The name cannot be more than 15 characters!"],
   },
   weight: {
-    type: String,
+    type: Number,
     required: [true, "Please add the Weight."],
     maxlength: [2, "Do not go above 2!"],
   },
   length: {
-    type: String,
+    type: Number,
     required: [true, "Please add the Length."],
     maxlength: [2, "Do not go above 2!"],
   },
