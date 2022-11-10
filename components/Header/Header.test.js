917be6c9ca9982ccc1c8c50927d47754f247337d
Patrick <pamarx92@gmail.com>
2022-11-10 17:28:32 +0100

    fixed a typo

--- components/Header/Header.test.js
+++ components/Header/Header.test.js
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 
-test("Displays me a Header", () => {
+test("displays a header ", () => {
   render(<Header />);
   const heading = screen.getByRole("heading", {
     name: /Capstone Project/i,
