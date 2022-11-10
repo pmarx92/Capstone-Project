f83fb6845d2a4ba051c90191fccfc935079db228
Patrick <pamarx92@gmail.com>
2022-11-10 11:27:31 +0100

    added a heading test

---
+++ components/Header/Header.test.js
@@ -0,0 +1,12 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Header from "./Header";
+
+test("Displays me a Header", () => {
+  render(<Header />);
+  const heading = screen.getByRole("heading", {
+    name: /Capstone Project/i,
+  });
+  expect(heading).toBeInTheDocument();
+});
