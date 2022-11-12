572e72a8d4bd89f55438b1a5e4d7350535c69030
Patrick <pamarx92@gmail.com>
2022-11-12 12:27:43 +0100

    added a fetch for the cloudinary images

--- pages/List/index.js
+++ pages/List/index.js
@@ -1,13 +1,45 @@
 import styled from "styled-components";
 import Link from "next/link";
 
+export async function getStaticProps() {
+  const results = await fetch(
+    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`,
+    {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          process.env.CLOUDINARY_API_KEY +
+            ":" +
+            process.env.CLOUDINARY_API_SECRET
+        ).toString("base64")}`,
+      },
+    }
+  ).then((r) => r.json());
+
+  const { resources } = results;
+
+  const images = resources.map((resource) => {
+    return {
+      id: resource.asset_id,
+      title: resource.public_id,
+      image: resource.secure_url,
+    };
+  });
+  return {
+    props: { images },
+  };
+}
+
 export default function List({ dataFromAPI }) {
   return (
     <div>
       <h1>This is the List Page - Under Construction</h1>
+
       {dataFromAPI.map((data) => {
+        console.log(data.cloudinarySrc);
         return (
           <Card key={data._id}>
+            <StyledImage src={data.cloudinarySrc} />
+
             <h2>
               <a>Name: {data.name}</a>
             </h2>
@@ -30,6 +62,9 @@ export default function List({ dataFromAPI }) {
     </div>
   );
 }
+const StyledImage = styled.img`
+  width: 50%;
+`;
 
 const Card = styled.div`
   display: flex;
--- pages/Map/index.js
+++ pages/Map/index.js
@@ -1,7 +1,44 @@
-export default function Map() {
+import styled from "styled-components";
+
+export async function getStaticProps() {
+  const results = await fetch(
+    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`,
+    {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          process.env.CLOUDINARY_API_KEY +
+            ":" +
+            process.env.CLOUDINARY_API_SECRET
+        ).toString("base64")}`,
+      },
+    }
+  ).then((r) => r.json());
+
+  const { resources } = results;
+  const images = resources.map((resource) => {
+    return {
+      id: resource.asset_id,
+      title: resource.public_id,
+      image: resource.secure_url,
+    };
+  });
+  return {
+    props: { images },
+  };
+}
+
+export default function Map({ images }) {
   return (
     <div>
-      <h1>This is the Map Page - Under Construction</h1>;
+      <h1>This is the Map Page - Under Construction</h1>
+      {images.map((image) => {
+        return <StyledImage src={image.image} />;
+      })}
     </div>
   );
 }
+const StyledImage = styled.img`
+  width: 15%;
+  height: 15%;
+  padding: 1rem;
+`;
