134139963b59ca75f93b37f7f46aa5185b8b97f7
Patrick <pamarx92@gmail.com>
2022-11-12 13:24:00 +0100

    fix for vercel?

---
+++ components/lib/cloudinary.js
@@ -0,0 +1,15 @@
+export async function cloudinary() {
+  const results = await fetch(
+    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`,
+    {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          process.env.CLOUDINARY_API_KEY +
+            ":" +
+            process.env.CLOUDINARY_API_SECRET
+        ).toString("base64")}`,
+      },
+    }
+  ).then((r) => r.json());
+  return results;
+}
--- pages/Map/index.js
+++ pages/Map/index.js
@@ -1,18 +1,8 @@
 import styled from "styled-components";
+import { cloudinary } from "../../components/lib/cloudinary";
 
 export async function getStaticProps() {
-  const results = await fetch(
-    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`,
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          process.env.CLOUDINARY_API_KEY +
-            ":" +
-            process.env.CLOUDINARY_API_SECRET
-        ).toString("base64")}`,
-      },
-    }
-  ).then((r) => r.json());
+  const results = await cloudinary();
 
   const { resources } = results;
   const images = resources.map((resource) => {
